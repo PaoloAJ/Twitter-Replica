33643e3183e90a533ba39382d94c64fbf3401aa3
Penzi_ <101235314+PaoloAJ@users.noreply.github.com>
2024-09-21 00:22:06 -0400

    Finished code: Styles next/Tailwind

--- client/src/pages/ChangePassword.js
+++ client/src/pages/ChangePassword.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function ChangePassword() {
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const changePassword = () => {
+    axios
+      .put(
+        "http://localhost:3001/users/changepassword",
+        {
+          oldPassword: oldPassword,
+          newPassword: newPassword,
+        },
+        {
+          headers: {
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        }
+      )
+      .then((response) => {
+        if (response.data.error) {
+          alert(response.data.error);
+        }
+      });
+  };
   return (
     <div>
       <h1>ChangePassword</h1>
-      <input type="text" placeholder="Old Password..." />
-      <input type="text" placeholder="New Password..." />
-      <button>Update Password</button>
+      <input
+        type="text"
+        placeholder="Old Password..."
+        onChange={(event) => {
+          setOldPassword(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="New Password..."
+        onChange={(event) => {
+          setNewPassword(event.target.value);
+        }}
+      />
+      <button onClick={changePassword}>Update Password</button>
     </div>
   );
 }
